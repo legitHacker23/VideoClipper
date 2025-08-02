0602e1f141909554154f5791c1038a99bae941c0
legitHacker23 <nbmendoza1432@gmail.com>
2025-08-02 12:19:20 -0500

    correct routes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://videoclipper-backend.onrender.com'
+      '/api': 'http://localhost:3001'
     }
   }
 })
